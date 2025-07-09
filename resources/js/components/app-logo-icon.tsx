919dce3d205dc0f84c6dc84d7a976710d661ef49
Sirotkin <nikolay.sirotkin@yvl.local>
2025-07-09 16:11:09 +0300

    Installed project and permissions

---
+++ resources/js/components/app-logo-icon.tsx
@@ -0,0 +1,9 @@
+import { SVGAttributes } from 'react';
+
+export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
+    return (
+        <svg {...props} xmlns="http://www.w3.org/2000/svg" width="64" height="32" viewBox="0 0 64 32" fill="none">
+            <path d="M0 -4.57764e-05H64L60.2215 1.96587L60.2143 15.2094L53.871 27.0627L56.2897 31.145L0 -4.57764e-05Z" fill="#ED1C24" />
+        </svg>
+    );
+}
