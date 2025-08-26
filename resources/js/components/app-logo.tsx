8bd75d62b6917af134b839f4b7d75e22cbaa5972
Sirotkin <nikolay.sirotkin@yvl.local>
2025-08-26 09:22:36 +0300

    Исправлен баг с логотипом

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,19 +1,13 @@
 import { SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
 import AppLogoIcon from './app-logo-icon';
-import { useSidebar } from './ui/sidebar';
 
 export default function AppLogo() {
     const { name } = usePage<SharedData>().props;
-    const { open } = useSidebar();
+
     return (
         <>
-            <div
-                className={
-                    'flex aspect-square items-center justify-center rounded-md text-sidebar-primary-foreground transition-all ' +
-                    (open ? 'size-12' : 'size-8')
-                }
-            >
+            <div className={'flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground transition-all'}>
                 <AppLogoIcon className="size-12 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-lg transition-all">
